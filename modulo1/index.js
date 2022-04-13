deaa85a05bbc8b8f48aee760fe2355d97f9a4774
Jefferson-Vieira <myjeff22vieira@mail.com>
2022-04-12 23:45:21 -0300

    index.js

---
+++ modulo1/index.js
@@ -0,0 +1,80 @@
+EXERCICIO  IINTERPRETAÇÃO DO CODIGO
+/* let a = 10 
+let b = 10
+console.log(b);
+b = 5 
+console.log(a,b);
+console.log("resposta variavel a =10 variavel b = 5"); */
+
+/* let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c);
+
+Resposta 10,10,10
+ */
+EXERCICIO  ESCRITA DE CODIGO 1
+/* 
+ let HoraTrabDia = prompt("Quantas horas você trabalha por dia?")
+let SalarioPorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${HoraTrabDia/SalarioPorDia} por hora`) */
+
+/*  let Nome
+let idade
+console.log(typeof Nome,typeof idade);
+undefined, undefined;
+ */ 
+/* let nome = prompt("Qual é o seu nome ?");
+let idade = prompt("Qual é a sua idade ?");
+let nome =  "Jefferson" 
+let idade = 34 
+console.log(typeof nome ,typeof idade);
+String, number 
+console.log("Olá", nome, "voçê tem", idade ,"anos."); */
+
+EXERCICIO ESCRITA DE CODIGO 1.2
+
+
+ /* let estaDeNoite = prompt("Estamos no horário noturno ?");
+let alunoLbenu = prompt("Voçê é aluno Labenu?");
+let mesAtual = prompt("Estamos em maio?");
+console.log("Estamos no horário noturno ?", estaDeNoite);
+console.log("Voçê é aluno Labenu?", alunoLbenu);
+console.log("Estamos em maio ?", mesAtual);
+ */
+
+
+
+EXERCICIO ESCRITA DE CODIGO 1.3
+/* let a = 10
+let b = 25
+let c = a 
+a = b ;
+b = c;
+
+console.log("O novo valor de a é", a); 
+ O novo valor de a é 25
+ console.log("O novo valor de b é", b);
+ O novo valor de b é 10 */
+
+
+ DESAFIO DO DIA 
+ let  numeroPar = prompt("Escreva um numero par",);
+ let numeroImpar = prompt("Escreva um numero impar",);
+ 
+ let ResultadoX = numeroPar.tonumber += numeroImpar.tonumber ;
+ let ResultadoY = numeroPar.tonumber * numeroImpar.tonumber;
+ console.log("Resultado da adição", numeroPar += numeroImpar,);
+ console.log("Resultado da multiplicação",numeroPar * numeroImpar,);
+
+
+
+
+
+
+
+
+
