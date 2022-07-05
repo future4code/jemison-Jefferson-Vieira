ea4e50af1e7eebc5442be4e74545519e78866a06
Jefferson-Vieira <myjeff22vieira@gmail.com>
2022-07-05 19:20:57 -0300

    exercicio-do-dia

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
-import foto1 from './img/img.beach.labenu.jpg';
-import foto2 from './img/img.universo.labetube.jpg';
-import djangoFoto from './img/img.django.jpg';
-import cristinaFoto from './img/img.cristina.jpg';
-
+ import foto1 from './img/img.beach.labenu.jpg';
+ import foto2 from './img/img.universo.labetube.jpg';
+ import djangoFoto from './img/img.django.jpg';
+ import cristinaFoto from './img/img.cristina.jpg';
+//  import { SecaoComentario } from './components/SecaoComentario/SecaoComentario';
+//  import { IconeComContador } from './components/IconeComContador/IconeComContador';
 
 
 const MainContainer = styled.div`
@@ -14,32 +15,61 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+  //    const arrayInsta = [
+  //    { 
+  //      nomeUsuario: "Paulinha",
+  //      fotoUsuario:'https://picsum.photos/50/50',
+  //      fotoPost:'https://picsum.photos/200/150',
+  //     },
+           
+  //    { 
+  //      nomeUsuario:"Django",
+  //      fotoUsuario: djangoFoto ,
+  //     fotoPost: foto1,
+  //   },
+
+  //  {
+  //     nomeUsuario: "Cristina" ,
+  //       fotoUsuario: cristinaFoto ,
+  //      fotoPost: foto2 ,
+  //      }
+  //    ]
+  //   const Usuarios = arrayInsta.map((usuario,index)=>{
+  //  return <Post key={index}>
+  //     <div>
+  //        {usuario.nomeUsuario}
+  //      {usuario.fotoUsuario}
+  //       </div>   
+  //     <div>
+  //      {usuario.fotoUsuario}
+  //      </div>  
+  //   </Post >
+
+  // })
+
+  // console.log({Usuarios})
 function App() {
+  
+  
 return(
   <MainContainer>
-
+           
            <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-          
-          <Post
-            nomeUsuario={'Django'}
-            fotoUsuario={djangoFoto}
-            fotoPost={foto1}
-          />
-
-             <Post
-            nomeUsuario={'Cristina'}
-            fotoUsuario={cristinaFoto}
-            fotoPost={foto2}
-          />
-
-        </MainContainer>
-)
-
-}
-
+               nomeUsuario={'Paulinha'}
+               fotoUsuario={'https://picsum.photos/50/50'}
+               fotoPost={'https://picsum.photos/200/150'}
+           />
+         <Post
+              nomeUsuario={'Django'}
+              fotoUsuario={djangoFoto}
+              fotoPost={foto1}
+    />
+     <Post
+               nomeUsuario={'Cristina'}
+               fotoUsuario={cristinaFoto}
+               fotoPost={foto2}
+    />
+       </MainContainer>
+)}
 
 export default App;
--- modulo3/estados-insta4/insta4/src/components/Post/Post.js
+++ modulo3/estados-insta4/insta4/src/components/Post/Post.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 
+
 import {IconeComContador} from '../IconeComContador/IconeComContador'
 
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
+// import djangoFoto from '../../img/img.django.jpg'
+// import foto1 from '../../img/img.beach.labenu.jpg'
+// import foto2 from '../../img/img.universo.labetube.jpg'
+// import cristinaFoto from '../../img/img.cristina.jpg'
+
 
 const PostContainer = styled.div`
   border: 1px solid gray;
@@ -49,7 +56,7 @@ function Post(props){
 
   const onClickCurtida = () => {
     setCurtido(!curtido)
-  if(numeroCurtidas == 0 ) {
+  if(numeroCurtidas === 0 ) {
     setnumeroCurtidas(numeroCurtidas+1)
   } else { 
     setnumeroCurtidas(numeroCurtidas -1)
@@ -90,7 +97,64 @@ function Post(props){
       componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
     }
 
+
+
+//     const arrayInsta = [
+//       { 
+//          nomeUsuario: "Paulinha",
+//          fotoUsuario:'https://picsum.photos/50/50',
+//          fotoPost:'https://picsum.photos/200/150',
+//        },
+             
+//       { 
+//          nomeUsuario:"Django",
+//          fotoUsuario: djangoFoto ,
+//          fotoPost: foto1 ,
+//        },
+  
+//       {
+//           nomeUsuario: "Cristina" ,
+//           fotoUsuario: cristinaFoto ,
+//           fotoPost: foto2 ,
+//         }
+//       ]
+//      const Usuarios = arrayInsta.map((usuario,index)=>{
+//       return <Post key={index}>
+        
+//         <PostContainer>
+//       <PostHeader>
+//         <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+//         <p>{usuario.nomeUsuario}</p>
+//       </PostHeader>
+
+//       <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+
+//       <PostFooter>
+//         <IconeComContador
+//           icone={iconeCurtida}
+//           onClickIcone={onClickCurtida}
+//           valorContador={numeroCurtidas}
+//         />
+ 
+//         <IconeComContador
+//           icone={iconeComentario}
+//           onClickIcone={onClickComentario}
+//           valorContador={numeroComentarios}
+
+//           // Os valores icone , onClickIcone e valor contador estados , eles vem de uma função e vão renderizar
+//           // a função no App.jS
+//         />
+//       </PostFooter>
+//       {componenteComentario}
+//     </PostContainer>
+
+//  </Post>
+//     })
+  
+    // console.log({Usuarios})
+
   return(
+    <Post> 
     <PostContainer>
       <PostHeader>
         <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
@@ -117,6 +181,7 @@ function Post(props){
       </PostFooter>
       {componenteComentario}
     </PostContainer>
+    </Post>
   )
 }
 
