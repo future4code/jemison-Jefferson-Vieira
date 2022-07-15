91a92ff63eb841560698bc8a9ae794b08811d2ba
Jefferson Vieira <myjeff22vieira@gmail.com>
2022-07-14 22:17:36 -0300

    exercicio-thinking

---
+++ modulo3/thinking-react/exercicio-e-comerce/src/MockDados.js
@@ -0,0 +1,47 @@
+
+export const dados=[
+    {
+        id: 1,
+        name: 'Produto legal 1',
+        price:100,
+        photo: 'https://picsum.photos/200/200?a=1',
+        status:false,
+      },
+      {
+        id: 1,
+        name: 'Produto legal 2',
+        price: 200,
+        photo: 'https://picsum.photos/200/200?a=1',
+        status:false,
+      },
+      {
+        id: 1,
+        name: 'Produto legal 3',
+        price:300,
+        photo: 'https://picsum.photos/200/200?a=1',
+        status:false,
+      },
+      {
+        id: 1,
+        name: 'Produto legal 4',
+        price:400,
+        photo: 'https://picsum.photos/200/200?a=1',
+        status:false,
+      },
+      {
+        id: 1,
+        name: 'Produto legal 5',
+        price: 500,
+        photo: 'https://picsum.photos/200/200?a=1',
+        status:false,
+      },
+      {
+        id: 1,
+        name: 'Produto legal 6',
+        price: 600,
+        photo: 'https://picsum.photos/200/200?a=1',
+        status:false,
+      },
+    
+
+]
